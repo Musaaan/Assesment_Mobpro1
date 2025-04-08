4cef62c856c8f6979af659fd1fb00df9c4375102
Muhammad Ihsan <mi5011487@gmail.com>
2025-04-08 14:21:22 +0700

    Menambahkan aset gambar bangun ruang

---
+++ app/src/main/java/com/musaan0129/assesment_mobpro1/model/BangunRuang.kt
@@ -0,0 +1,10 @@
+package com.musaan0129.assesment_mobpro1.model
+
+import android.adservices.adid.AdId
+import android.media.Image
+import androidx.annotation.DrawableRes
+
+data class BangunRuang(
+    val nama: String,
+    @DrawableRes val imageResId: Int
+)
